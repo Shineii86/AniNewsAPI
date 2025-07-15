3c8c5d4648e3d1b5e93503405b72f9d120d70ecb
シンエイ <157171073+Shineii86@users.noreply.github.com>
2025-07-16 04:45:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  try {
-    const filePath = path.join(process.cwd(), "public", "index.html");
-    const html = fs.readFileSync(filePath, "utf8");
+  const filePath = path.join(process.cwd(), "public", "index.html");
+  const html = fs.readFileSync(filePath, "utf8");
 
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(html);
-  } catch (err) {
-    res.status(500).json({
-      message: "Welcome to AniNewsAPI!",
-      creator: "Shinei Nouzen",
-      github: "https://github.com/Shineii86",
-      telegram: "https://telegram.me/Shineii86",
-      info: "Built with ❤️ by Shinei Nouzen",
-      timestamp: new Date().toLocaleString("en-IN", {
-        timeZone: "Asia/Kolkata",
-        hour12: true,
-      })
-    });
-  }
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.status(200).send(html);
 }
